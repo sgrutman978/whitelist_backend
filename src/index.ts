1673335747d23186a377e93f4bf764ea91cabcf4
sgrutman978 <sgrutman978@gmail.com>
2025-02-28 17:25:36 +0000

    server changes cors

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
-import { SuiClient } from '@mysten/sui/dist/cjs/client';
+import { SuiClient } from '@mysten/sui/client';
 import express, { Request, Response } from 'express';
 import { getBalances } from './Utility';
-    
+   
+import cors from 'cors';
+
+
 export const suiClient = new SuiClient({ url: "https://go.getblock.io/2e301c1cd2e542e897c7a14109d81baf" }); //getFullnodeUrl("mainnet")
 export const OneCoinNineDecimals = 1000000000;
 
 
 const app = express();
-const port = 3000;
+const port = 3006;
+app.use(cors())
 
 app.get('/getAddyStuffs', (req: Request, res: Response) => {
     const addy = req.query.addy as string;
     getBalances(addy).then(data => {
-        res.send(data);
+            let json = JSON.stringify(Object.fromEntries(data));
+	    console.log(json);
+	    res.send(json);
     })
 });
 
